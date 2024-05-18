d0e296b06750d107e8b758adb62421b384f80ff1
Ondrej Necas <ondrej.necas@second-foundation.eu>
2024-05-18 15:21:37 +0200

    grid locking

--- hw9-Efficient-server/server-src/config.cmake
+++ hw9-Efficient-server/server-src/config.cmake
@@ -4,6 +4,8 @@ option(ENABLE_INFO_LOG "Enable info logging level" ON)
 option(ENABLE_WARN_LOG "Enable warn logging level" ON)
 option(ENABLE_ERROR_LOG "Enable error logging level" ON)
 
+option(ENABLE_LOCKING "Enable grid locking" ON)
+
 # Conditionally add definitions based on the configuration option
 if(ENABLE_DEBUG_LOG)
     add_definitions(-DENABLE_DEBUG)
@@ -16,4 +18,7 @@ if(ENABLE_WARN_LOG)
 endif()
 if(ENABLE_ERROR_LOG)
     add_definitions(-DENABLE_ERROR)
+endif()
+if(ENABLE_LOCKING)
+    add_definitions(-DENABLE_LOCKING)
 endif()
