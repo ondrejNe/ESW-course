9e16c83c76ac1aeb994c52b02dc8d6c561f90355
Ondrej Necas <ondrej.necas@second-foundation.eu>
2024-05-20 01:23:31 +0200

    optimalization

--- hw9-Efficient-server/server-src/grid/GridProtobuf.cpp
+++ hw9-Efficient-server/server-src/grid/GridProtobuf.cpp
@@ -10,23 +10,43 @@ void Grid::processWalk(const esw::Walk &walk) {
     protoLogger.info("Walk message - %d locations - %d lengths", locations.size(), lengths.size());
 //    vector<future<uint64_t>> futures;
 
-    for (int i = 0; i < locations.size() - 1; ++i) {
-        const auto &location1 = locations.Get(i);
-        const auto &location2 = locations.Get(i + 1);
-        const auto &length = lengths.Get(i);
+    auto &location1 = locations.Get(0);
+    auto &location2 = locations.Get(1);
+    auto &length = lengths.Get(0);
 
+    Point origin = {static_cast<uint64_t>(location1.x()), static_cast<uint64_t>(location1.y())};
+    Point destination = {static_cast<uint64_t>(location2.x()), static_cast<uint64_t>(location2.y())};
+
+    string originCellId = getPointCellId(origin);
+    addPoint(origin, originCellId);
+
+    string destinationCellId = getPointCellId(destination);
+    addPoint(destination, destinationCellId);
+
+    addEdge(originCellId, destinationCellId, length);
+
+    for (int i = 1; i < locations.size() - 1; ++i) {
+        auto &location = locations.Get(i + 1);
+        auto &len = lengths.Get(i);
+
+        destination = {static_cast<uint64_t>(location.x()), static_cast<uint64_t>(location.y())};
+
+        originCellId = destinationCellId;
+        destinationCellId = getPointCellId(destination);
+        addPoint(destination, destinationCellId);
+        addEdge(originCellId, destinationCellId, len);
 //        futures.emplace_back(resourcePool.run([this, location1, location2, length]() -> uint64_t {
 
-            Point origin = {static_cast<uint64_t>(location1.x()), static_cast<uint64_t>(location1.y())};
-            Point destination = {static_cast<uint64_t>(location2.x()), static_cast<uint64_t>(location2.y())};
-
-            string originCellId = getPointCellId(origin);
-            addPoint(origin, originCellId);
-
-            string destinationCellId = getPointCellId(destination);
-            addPoint(destination, destinationCellId);
-
-            addEdge(originCellId, destinationCellId, length);
+//            Point origin = {static_cast<uint64_t>(location1.x()), static_cast<uint64_t>(location1.y())};
+//            Point destination = {static_cast<uint64_t>(location2.x()), static_cast<uint64_t>(location2.y())};
+//
+//            string originCellId = getPointCellId(origin);
+//            addPoint(origin, originCellId);
+//
+//            string destinationCellId = getPointCellId(destination);
+//            addPoint(destination, destinationCellId);
+//
+//            addEdge(originCellId, destinationCellId, length);
 
 //            return 0;
 //        }));
