9e375cddfb23511a7e445c3498002b8ff2d6d505
Ondrej Necas <ondrej.necas@second-foundation.eu>
2024-05-24 22:29:14 +0200

    cell size debug

--- hw9-Efficient-server/server-src/grid/GridApi.cpp
+++ hw9-Efficient-server/server-src/grid/GridApi.cpp
@@ -55,6 +55,7 @@ void Grid::addPoint(Point &point, uint64_t &cellId) {
         Point p = {point.x, point.y};
         Cell newCell(id, coordX, coordY, p);
         cells[id] = newCell;
+        apiLogger.info("Total cells: %d", cells.size());
     }
 }
 
