4eca71a21dab68a1a562097242f4f50bf6226991
Ondrej Necas <ondrej.necas@second-foundation.eu>
2024-06-17 23:18:43 +0200

    quickfix

--- hw9-Efficient-server/server-src/logger/Logger.hh
+++ hw9-Efficient-server/server-src/logger/Logger.hh
@@ -66,7 +66,6 @@ public:
     // Log a formatted message
     template<typename... Args>
     void log(LogLevel level, string formatString, Args... args) {
-        if (!active) return;
 #ifndef ENABLE_DEBUG
         if (level == DEBUG) return;
 #endif
