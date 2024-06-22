b25baf747d4aec4f90495e48c11ea2baf66838ff
Ondrej Necas <ondrej.necas@second-foundation.eu>
2024-06-22 20:21:58 +0200

    stats

--- hw9-Efficient-server/server-src/grid/GridSearch.cpp
+++ hw9-Efficient-server/server-src/grid/GridSearch.cpp
@@ -8,6 +8,7 @@ PrefixedLogger searchLogger = PrefixedLogger("[SEARCHING]", true);
 PrefixedLogger searchStatsLogger = PrefixedLogger("[SEARCH STATS]", true);
 
 #ifdef SEARCH_STATS_LOGGER
+uint64_t maxSums = 0;
 uint64_t maxEdges = 0;
 uint64_t maxPqSize = 0;
 #endif
@@ -39,6 +40,11 @@ uint64_t Grid::dijkstra(uint64_t &originCellId, uint64_t &destinationCellId, boo
 
     // Main loop of Dijkstra's Algorithm
     while (!pq.empty()) {
+#ifdef SEARCH_STATS_LOGGER
+        if (pq.size() > maxPqSize) {
+            maxPqSize = pq.size();
+        }
+#endif
         // Get the cell with the minimum distance from the priority queue
         uint64_t originCurrent = pq.top().first;
         uint64_t currentCellId = pq.top().second;
@@ -48,16 +54,28 @@ uint64_t Grid::dijkstra(uint64_t &originCellId, uint64_t &destinationCellId, boo
         visited[currentCellId] = 1;
 
         if (currentCellId == destinationCellId && !oneToAll) {
-            return originCurrent;
+            sum = originCurrent;
+            break;
         } else {
             sum += originCurrent;
+#ifdef SEARCH_STATS_LOGGER
+            maxSums++;
+#endif
         }
 
+#ifdef SEARCH_STATS_LOGGER
+        if (cells[currentCellId].edges.size() > maxEdges) {
+            maxEdges = cells[currentCellId].edges.size();
+        }
+#endif
         for (const auto &[neighborCellId, edge]: cells[currentCellId].edges) {
             if (visited[neighborCellId] == 1) continue;
             pq.push({originCurrent + edge, neighborCellId});
         }
     }
 
+#ifdef SEARCH_STATS_LOGGER
+    searchStatsLogger.warn("Max sums: %lu Max edges: %lu Max PQ size: %lu", maxSums, maxEdges, maxPqSize);
+#endif
     return sum;
 }
