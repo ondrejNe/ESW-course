19458ef7cb368f57bc5d64f9a07e6c953a4eff89
Ondrej Necas <ondrej.necas@second-foundation.eu>
2024-06-28 17:24:18 +0200

    one thread

--- hw9-Efficient-server-kt/src/main/kotlin/Main.kt
+++ hw9-Efficient-server-kt/src/main/kotlin/Main.kt
@@ -15,7 +15,7 @@ import java.net.Socket
 import java.util.concurrent.Executors
 
 fun main() = runBlocking {
-    val threadCount = 30
+    val threadCount = 1
     val executor = Executors.newFixedThreadPool(threadCount)
     val dispatcher = executor.asCoroutineDispatcher()
 
